248acbc8e6f71d1622eb1ca26b29b526acbac3f7
rodrigotoledodm <rodrigomeloarthur@gmail.com>
2025-07-03 16:34:56 -0300

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,9 @@
+const ground = {
+    color:"green",
+    width: 320,
+    height: 90,
+    draw(){
+        eCtx.fillStyle = this.color;
+        eCtx.fillRect(0,450,this.width,this.height)
+    }
+}
